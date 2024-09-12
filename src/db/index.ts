cecf0a70e973e5983dd942cc26244970e1895bfd
taynara veloso <taynarasantos2.3@hotmail.com>
2024-09-12 19:52:58 -0300

    :star: db configs/migrates

---
+++ src/db/index.ts
@@ -0,0 +1,7 @@
+import { env } from '../env'
+import postgres from 'postgres'
+import * as schema from './schema'
+import { drizzle } from 'drizzle-orm/postgres-js'
+
+export const client = postgres(env.DATABASE_URL)
+export const db = drizzle(client, { schema, logger: true })
---
+++ src/db/seed.ts
@@ -0,0 +1,30 @@
+import dayjs from 'dayjs'
+import { client, db } from '.'
+import { goalCompletions, goals } from './schema'
+
+async function seed() {
+  await db.delete(goalCompletions)
+  await db.delete(goals)
+
+  const result = await db
+    .insert(goals)
+    .values([
+      { title: 'Acordar cedo', desiredWeeklyFrequency: 5 },
+      { title: 'Meditar', desiredWeeklyFrequency: 5 },
+      { title: 'Me exercitar pela manhã', desiredWeeklyFrequency: 5 },
+      { title: 'Caminhada', desiredWeeklyFrequency: 2 },
+      { title: 'Estudar', desiredWeeklyFrequency: 6 },
+    ])
+    .returning()
+
+  const startOfWeek = dayjs().startOf('week')
+
+  await db.insert(goalCompletions).values([
+    { goalId: result[0].id, createdAt: startOfWeek.toDate() },
+    { goalId: result[2].id, createdAt: startOfWeek.add(1, 'day').toDate() },
+  ])
+}
+
+seed().finally(() => {
+  client.end()
+})
