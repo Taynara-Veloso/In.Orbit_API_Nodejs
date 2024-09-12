c374a3866d7c9d018ea9b93955cbb137c7893285
taynara veloso <taynarasantos2.3@hotmail.com>
2024-09-12 19:55:01 -0300

    :star: create goal completion

---
+++ src/functions/create-goal-completion.ts
@@ -0,0 +1,39 @@
+import dayjs from 'dayjs'
+import { db } from '../db'
+import { goalCompletions, goals } from '../db/schema'
+import { and, count, eq, gte, lte, sql } from 'drizzle-orm'
+
+interface CreateGoalCompletionRequest {
+  goalId: string
+}
+
+export async function CreateGoalCompletion({
+  goalId,
+}: CreateGoalCompletionRequest) {
+  const firstDayOfWeek = dayjs().startOf('week').toDate()
+  const lastDayOfWeek = dayjs().endOf('week').toDate()
+
+  const goalCompletionCounts = db.$with('goal_completion_counts').as(
+    db
+      .select({
+        goalId: goalCompletions.goalId,
+        completionCount: count(goalCompletions.id).as('completionCount'),
+      })
+      .from(goalCompletions)
+      .where(
+        and(
+          gte(goalCompletions.createdAt, firstDayOfWeek),
+          lte(goalCompletions.createdAt, lastDayOfWeek),
+          eq(goalCompletions.goalId, goalId)
+        )
+      )
+      .groupBy(goalCompletions.goalId)
+  )
+
+  const result = await db.with(goalCompletionCounts).select({
+    desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+    completionCount: sql`
+      COALESCE(${goalCompletionCounts.completionCount}, 0)
+    `
+  })
+}
